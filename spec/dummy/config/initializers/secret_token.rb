2bf2bb4e5754e82fa446413f1aba3d6acae94d31
Ryan Muller <cognitionmachine@gmail.com>
2012-02-06 21:28:40 -0500

    added dummy app for testing

---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '4e207775a05da114b4d3a33caabd9fa1b4435128605d11868869c9631f6cd00998099ba7ab2a11233c7a7c9f2adbd124d277fe7750f2981d7725f765d147ceb3'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
+# Configure Rails Environment
+ENV["RAILS_ENV"] = "test"
+
 require "bundler"
 Bundler.setup
 
@@ -5,6 +8,11 @@ require "rspec"
 require "spaceable"
 #require "support/matchers"
 
+require File.expand_path("../dummy/config/environment.rb",  __FILE__)
+
+Rails.backtrace_cleaner.remove_silencers!
+
 Rspec.configure do |config|
   #config.include NewGem::Spec::Matchers
 end
+
